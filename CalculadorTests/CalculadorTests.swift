1cda1516efee8746db55500049668aefff04b8f3
David Pulloquinga <david@pulloquinga.com>
2015-04-28 17:57:30 -0500

    Initial Commit

---
+++ CalculadorTests/CalculadorTests.swift
@@ -0,0 +1,36 @@
+//
+//  CalculadorTests.swift
+//  CalculadorTests
+//
+//  Created by David Pulloquinga on 4/28/15.
+//  Copyright (c) 2015 David Pulloquinga. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CalculadorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
