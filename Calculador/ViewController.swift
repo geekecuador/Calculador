fef4b26fc9241d79adf43279ce0a91d9de0aaab7
David Pulloquinga <david@pulloquinga.com>
2015-04-28 19:34:12 -0500

    Calculadora primera parte
    Calculadora en Swift

--- Calculador/ViewController.swift
+++ Calculador/ViewController.swift
@@ -10,9 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var edt_resultado: UITextField!
+    var tipiando = false
+    var punto = false
+    var alerta = UIAlertController(title: "Error", message: "No se permite más de un punto", preferredStyle: .Alert)
+    
+    @IBAction func asignar(sender: UIButton) {
+        let digito = sender.currentTitle!
+        if tipiando{
+            edt_resultado.text = edt_resultado.text! + digito
+        }
+        else
+        {
+            edt_resultado.text = digito
+            tipiando = true
+            punto = true
+        }
+
+    }
+    
+    @IBAction func asignarPunto(sender: UIButton) {
+        if punto{
+            edt_resultado.text = edt_resultado.text! + "."
+            punto = false
+        }
+        else
+        {
+            
+            presentViewController(alerta, animated: true, completion: nil)
+        }
+        
+    }
+    
+    
+    @IBAction func operar(sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alerta.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
     }
 
     override func didReceiveMemoryWarning() {
